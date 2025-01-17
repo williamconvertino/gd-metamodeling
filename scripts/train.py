ac78addcd62b97ad470eda1d898c6e3bc8c25ec0
William Convertino <williamconvertino@gmail.com>
2025-01-17 01:30:06 -0500

    debugging

--- scripts/train.py
+++ scripts/train.py
@@ -17,6 +17,8 @@ if __name__ == "__main__":
     flags = get_flags_from_args()
     if 'cpu' in flags:
         device = torch.device('cpu')
+        
+    print(model.config)
     
     tokenizer = get_tokenizer()
     model.resize_vocabulary(len(tokenizer))
