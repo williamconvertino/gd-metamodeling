36709e6d05c373a888d20b61a7a10605514ed1fb
William Convertino <williamconvertino@gmail.com>
2025-01-29 16:51:10 -0500

    Updated batch id

--- src/evaluation/gpt4o_eval.py
+++ src/evaluation/gpt4o_eval.py
@@ -12,7 +12,7 @@ OUTPUT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../dat
 
 MODEL = 'gpt-4o'
 FILE_NAME = f"{MODEL}_eval_input.jsonl"
-BATCH_ID = 'batch_679a9a689ae48190b6d74628f950f835'
+BATCH_ID = 'batch_679a82d2f1ac8190b38d0d4da50b635e'
 
 def load_api():
   env_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../.env')
